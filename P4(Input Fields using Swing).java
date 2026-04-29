dd9e78e5c2e34fac13810af4e63323cede054718
arjun <88441752+immortaldotjar@users.noreply.github.com>
2026-04-29 16:48:57 +0530

    Create P4(Input Fields using Swing).java

---
+++ P4(Input Fields using Swing).java
@@ -0,0 +1,27 @@
+import java.awt.*;
+import javax.swing.*;
+public class p3 {
+    public static void main(String[] args) {
+        // Containers
+        JFrame f = new JFrame("Swing");
+        f.setSize(500, 500);
+        f.setLayout(new FlowLayout()); 
+        //User inputs
+        JTextField txt = new JTextField("Enter text", 20);
+        JCheckBox cb = new JCheckBox("I agree");
+        // Radio button creation
+        JRadioButton rbtn1 = new JRadioButton("Apple"); 
+        JRadioButton rbtn2 = new JRadioButton("Banana");
+        //Grouping radio buttons
+        ButtonGroup grp = new ButtonGroup();
+        grp.add(rbtn1);
+        grp.add(rbtn2);
+        // Adding components to frame
+        f.add(txt);
+        f.add(cb);
+        f.add(rbtn1);
+        f.add(rbtn2);
+        f.setVisible(true);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Exit on Close
+    }
+}
